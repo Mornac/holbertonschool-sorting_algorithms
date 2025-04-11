87bc6aec89896f2220f0b6d0e90c0f3870173828
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-04-11 10:29:15 +0000

    Fixing the excepted checker , call the function array and deleting the printf at the end

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,23 +3,18 @@
 #include "sort.h"
 
 /**
- *bubble_sort - sorts an array of integers in ascending order
- *using the Bubble sort algorithm
- *@array: array of integers
- *@size: size of the array
- *Return: array in ascending order
+ * bubble_sort - sorts an array of integers in ascending order
+ * using the Bubble sort algorithm
+ * @array: array of integers
+ * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
-	size_t j;
-	size_t k;
+	size_t i, j;
 	int temp;
 
 	if (array == NULL || size < 2)
-	{
 		return;
-	}
 
 	for (i = 0; i < size - 1; i++)
 	{
@@ -31,9 +26,7 @@ void bubble_sort(int *array, size_t size)
 				array[j] = array[j + 1];
 				array[j + 1] = temp;
 
-			for (k = 0; k < size; k++)
-				printf("%d ", array[k]);
-			printf("\n");
+				print_array(array, size); /* Le checker attendait ca Ingrid */
 			}
 		}
 	}
